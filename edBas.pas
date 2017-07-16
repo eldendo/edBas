eff286a908e5968e9479186ae6d38fcfdfbb2b6b
eldendo <marc.dendooven@gmail.com>
2017-07-16 23:52:38 +0200

    Add files via upload

--- edBas.pas
+++ edBas.pas
@@ -1,25 +1,45 @@
 (* a very simple basic interpreter
 * (c)2017 by ir. Marc Dendooven *)
-
 program edBas;
 uses sysutils;
 
+const lastLine = 9999; 
+
 var line: string;
+    lp: integer; // pointer in 'line'
+    prog: array[1..lastLine] of string;
+    quit: boolean = true;
+    
+    function number: integer;
+    begin
+	number := 0;    
+	while (line[lp]) in ['0'..'9'] do begin number := number*10+ord(line[lp])-ord('0'); inc(lp) end;
+    end;
     
     procedure addLine;
     begin
-	writeln('add line: ',line)
+	prog[number] := trim(rightStr(line,length(line)-lp))
     end;
     
     procedure execLine;
+    
+	procedure list;
+	var i: integer;
+	begin
+		for i := 1 to lastLine do if prog[i]<>'' then writeln(i,' ',prog[i])
+	end;
+    
     begin
- 	writeln('execute line: ',line)   
+	if      leftstr(line,4) = 'LIST' then list
+	else if leftstr(line,4) = 'EXIT' then begin writeln('bye'); quit := true; end
+	else if leftstr(line,3) = 'REM' then // do nothing
+	else writeln('SYNTAX ERROR') 
     end;
 
     procedure evaluate;
     begin
-	line := trim(line);
-	if line[1] in ['0'..'9'] then addLine else execLine
+	line := upCase(trim(line)); lp := 1;
+	if line[lp] in ['0'..'9'] then addLine else execLine
     end;
 
 begin
@@ -31,8 +51,8 @@ begin
     writeln;
     
     repeat // Read Evaluate Loop
-	writeln('READY.'); write('>');
+	write('>');
 	readln(line);
 	evaluate
-    until false 
+    until quit 
 end. 
